130ad4b22c93b96d36995cff77de76ed2eed9e70
Yusdivia Molina <yus.molina@gmail.com>
2022-09-07 21:15:08 -0500

    nombres

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import Screen_Alg1 from './screens/Screen_Alg1';
+import Screen_Alg2 from './screens/Screen_Alg2';
+import PredictionScreen from './screens/PredictionScreen';
+import SelectionScreen from './screens/SelectionScreen';
+
+
+const Slack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <TailwindProvider>
+          <Slack.Navigator>
+            <Slack.Screen name='Home' component={HomeScreen}/>
+          </Slack.Navigator>
+        </TailwindProvider>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+        <Text>Paola Fernández Gutiérrez Zamora - A01658087</Text>
+        <Text>Yusdivia Molina Román - A01653120</Text>
+        <Text>Miguel A. Medina R. - A01023656</Text>
+        <Text>Annya Paulina Verduzco Meza - A01650668</Text>
+    </View>
+  )
+}
