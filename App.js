be3aeec116e3da01a8062153887cb5fbd872133d
paofgz <a01658087@tec.mx>
2022-09-12 17:40:05 -0500

    finished screen

--- App.js
+++ App.js
@@ -8,18 +8,30 @@ import Screen_Alg1 from './screens/Screen_Alg1';
 import Screen_Alg2 from './screens/Screen_Alg2';
 import PredictionScreen from './screens/PredictionScreen';
 import SelectionScreen from './screens/SelectionScreen';
-
+import { DefaultTheme, Provider } from 'react-native-paper'
 
 const Slack = createNativeStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#275DAD',
+    secondary: '#ABA9C3',
+    tertiary: '#5B616A'
+  },
+};
 
 export default function App() {
   return (
       <NavigationContainer>
         <TailwindProvider>
-          <Slack.Navigator>
-            
-            <Slack.Screen name='Prediction' component={PredictionScreen} options={{headerShown: false}}/>
-          </Slack.Navigator>
+          <Provider theme={theme}>
+            <Slack.Navigator>
+              
+              <Slack.Screen name='Prediction' component={PredictionScreen} options={{headerShown: false}}/>
+            </Slack.Navigator>
+          </Provider>
         </TailwindProvider>
       </NavigationContainer>
   );
