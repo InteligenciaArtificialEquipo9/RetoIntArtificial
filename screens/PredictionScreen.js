4c281cf738bbf50243b50e4ea11ee079780df311
paofgz <a01658087@tec.mx>
2022-09-13 18:21:28 -0500

    Fixed prediction

--- screens/PredictionScreen.js
+++ screens/PredictionScreen.js
@@ -8,59 +8,92 @@ export default function PredictionScreen() {
 
   const [state, setState] = useState({
     name: '',
-    age: 0,
     socialClass: '0', 
-    gender: ''
+    gender: '',
+    embarked: '',
+    fare: 0
   })
 
   const predict = () => {
     console.log(state)
+    if(!state.name) {
+      alert('Please enter your name')
+      return
+    }
+    if(!state.gender) {
+      alert('Please select your gender')
+      return 
+    }
+    if (!state.socialClass) {
+      alert('Please select a class to travel on')
+      return
+    }
+    if (!state.embarked) {
+      alert('Please select your port')
+      return
+    }
+    if (!state.fare) {
+      alert('Please enter the cost of your ticket')
+      return
+    }
     // Aqui se va a mandar lo de state para predecir
     alert('Mueres')
+    return
   }
 
   return (
     <>
     <View className='pt-5 h-full flex-1 bg-[#EEF1F7]'>
       <View className='items-center'>
-        <Text className='text-xl font-bold mt-5'>¿Sobrebvivirías?</Text>
-        <Text className='text-lg text-gray-700'>Ingresa tus datos y te daremos la respuesta</Text>
+        <Text className='text-xl font-bold mt-5'>Would you survive?</Text>
+        <Text className='text-lg text-gray-700'>Enter your data and find out.</Text>
       </View>
       <ScrollView 
       className='w-screen pl-3 pr-2 mb-20'>
-        <Text className='text-lg mb-2 mt-8 font-bold'>Nombre</Text>
+        <Text className='text-lg mb-2 mt-8 font-bold'>Your name</Text>
         <TextInput 
         value={state.name}
         onChangeText={name => setState({...state, name})}
         />
-        <Text className='text-lg mb-2 mt-8 font-bold'>Edad</Text>
-        <TextInput 
-        value={state.age}
-        onChangeText={age => setState({...state, age})}
-        keyboardType='number-pad'
-        />
-        <Text className='text-lg mb-2 mt-8 font-bold'>Genero</Text>
+        <Text className='text-lg mb-2 mt-8 font-bold'>Your gender</Text>
         <RadioButton.Group
         value={state.gender}
         onValueChange={gender => setState({...state, gender})}>
             <RadioButton.Item label='Mujer' value='M' />
             <RadioButton.Item label='Hombre' value='H' />
         </RadioButton.Group>
-        <Text className='text-lg mt-8 font-bold'>Clase</Text>
+        <Text className='text-lg mt-8 font-bold'>What class will you travel on?</Text>
         <View className='items-center relative'>
           <Picker
           className='items-center'
           selectedValue={state.socialClass}
-          style={{ height: 50, width: 275 }}
+          style={{ height: 30, width: 275 }}
           onValueChange={(socialClass) => setState({...state, socialClass})}>
-            <Picker.Item label='Baja' value='0' />
-            <Picker.Item label='Media baja' value='1' />
-            <Picker.Item label='Media alta' value='2' />
-            <Picker.Item label='Alta' value='3' />
+            <Picker.Item label='Economy Class' value='1' />
+            <Picker.Item label='Business Class' value='2' />
+            <Picker.Item label='First Class' value='3' />
           </Picker>
         </View>
-        <TouchableOpacity className='mt-40'>
-          <Button onPress={predict}>Predecir</Button>
+        <Text className='text-lg mt-44 font-bold'>On which port will you embark?</Text>
+        <View className='items-center relative'>
+          <Picker
+          className='items-center'
+          selectedValue={state.embarked}
+          style={{ height: 30, width: 275 }}
+          onValueChange={(embarked) => setState({...state, embarked})}>
+            <Picker.Item label='Cherbourg' value='0' />
+            <Picker.Item label='Queenstown' value='1' />
+            <Picker.Item label='Southampton' value='2' />
+          </Picker>
+        </View>
+        <Text className='text-lg mb-2 mt-44 font-bold'>How much did you pay for your ticket?</Text>
+        <TextInput 
+        value={state.fare}
+        onChangeText={fare => setState({...state, fare})}
+        keyboardType='number-pad'
+        />
+        <TouchableOpacity className='mt-10'>
+          <Button onPress={predict}>Predict</Button>
         </TouchableOpacity>
       </ScrollView>
 
